e116d2e31e1d0a2e20df7b05fdc521f3ddfb6a25
phoenix-gl <guangli901030@hotmail.com>
2020-08-08 00:01:16 +0800

    Initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,5 @@ export class AppComponent {
   settings: any;
   constructor(private resService: ResService) {
     setTheme('bs4');
-    this.resService.loadSettings();
   }
 }
--- src/app/res.service.ts
+++ src/app/res.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import {ResItem} from './models/res-item';
 
 const electron = (window as any).require('electron');
 
