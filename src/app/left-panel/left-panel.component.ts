020b954de238c9831168bb42b54fbacb5d87ea70
phoenix-gl <guangli901030@hotmail.com>
2020-08-20 13:09:11 +0800

    Fix output html

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -108,6 +108,9 @@ export class LeftPanelComponent implements OnInit, OnDestroy {
       }
 
       const newItem = new TwitItem();
+      newItem.container = '0';
+      newItem.picture = '0';
+      newItem.photos = [];
       const embedResponse = await fetch(`https://publish.twitter.com/oembed?hide_thread=true&align=center&omit_script=true&url=${twitter}`);
       if (embedResponse.ok) {
         const data = await embedResponse.json();
@@ -145,7 +148,6 @@ export class LeftPanelComponent implements OnInit, OnDestroy {
               }
             }
           }
-          console.log(apiData);
           this.twitList.push(newItem);
         }
       }
@@ -253,11 +255,32 @@ export class LeftPanelComponent implements OnInit, OnDestroy {
       line += `<span class="t_bird_icon"><a href="${twit.url}" target="_blank"><img src="${this.settings.url}tw_bird.png"></a></span></div><!-- e-t_header -->`
       line += `<div class="t_honbun">\n`;
       line += twit.text + '\n';
-      line += `<div class="t_media"><!-- s-img -->`
-      for (const photo of twit.photos){
-        line += `<a href="${photo.url}" class="swipe" rel="${twit.id}" title="${this.settings.title_fukusuu}" target="_blank"><img src="${photo.url}" class="no_image" width="${value.imageWidth}"></a>\n`;
+      if(twit.photos.length > 0) {
+        if (Number(twit.picture) > 1){
+          line += `<div class="t_media${twit.picture}"><!-- s-img -->`
+        } else{
+          if (twit.picture !== '1' && value.imageType > 1){
+            line += `<div class="t_media${value.imageType}"><!-- s-img -->`
+          }else{
+            line += `<div class="t_media"><!-- s-img -->`
+          }
+        }
+
+        for (const photo of twit.photos) {
+          if (twit.picture === '2' || (twit.picture === '0' && value.imageType === 2)){
+            line += '<div>'
+          }
+          line += `<a href="${photo.url}" class="swipe" rel="${twit.id}" title="${this.settings.title_fukusuu}" target="_blank"><img src="${photo.url}" class="no_image"`;
+          if (twit.picture === '1' || (twit.picture === '0' && value.imageType === 1)){
+            line += ` width="${value.imageWidth}"`
+          }
+          line += `></a>\n`;
+          if (twit.picture === '2' || (twit.picture === '0' && value.imageType === 2)){
+            line += '</div>'
+          }
+        }
+        line += `<!-- e-img --></div><!-- e_t_media -->\n`;
       }
-      line += `<!-- e-img --></div><!-- e_t_media -->\n`;
       line += `</div><!-- e-t_honbun -->\n`;
       line += `<div class="t_footer"><div class="t_buttons">\n`;
       line += `<a class="t_reply_button" href="https://twitter.com/intent/tweet?in_reply_to=${twit.id}"><img src="${this.settings.url}tw_icon1.png"></a>\n`;
@@ -265,7 +288,7 @@ export class LeftPanelComponent implements OnInit, OnDestroy {
       line += `<a class="t_fav_button" href="https://twitter.com/intent/favorite?tweet_id=${twit.id}"><img src="${this.settings.url}tw_icon3.png"></a>\n`;
       line += `</div><!-- e-t_buttons -->\n`;
       let createdDate = new Date(twit.createdAt);
-      let formattedDate = `${createdDate.getFullYear()}-${(createdDate.getMonth() + 1)}-${createdDate.getDate()} ${createdDate.getHours()}:${createdDate.getMinutes()}`;
+      let formattedDate = `${createdDate.getFullYear()}-${("0" + (createdDate.getMonth() +　1)).slice(-2)}-${("0" + (createdDate.getDay())).slice(-2)} ${("0" + (createdDate.getHours())).slice(-2)}:${("0" + (createdDate.getMinutes())).slice(-2)}`;
       line += `<div class="t_date"><a href="${twit.url}" target="_blank">${formattedDate}</a></div>\n`;
       line += `</div><!-- e-t_footer --></div><!-- e-t_container -->`;
       output += line;
