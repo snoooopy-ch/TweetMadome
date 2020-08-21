13245a0da9480373fc80b2c5d173678b0b6cb40d
phoenix-gl <guangli901030@hotmail.com>
2020-08-21 16:39:06 +0800

    Fix emoji

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -369,14 +369,15 @@ export class LeftPanelComponent implements OnInit, OnDestroy {
   }
 
   getEmojiCode (emoji) {
-    var comp;
+    let comp;
     if (emoji.length === 1) {
       comp = emoji.charCodeAt(0);
+    } else {
+      comp = (
+        (emoji.charCodeAt(0) - 0xD800) * 0x400
+        + (emoji.charCodeAt(1) - 0xDC00) + 0x10000
+      );
     }
-    comp = (
-      (emoji.charCodeAt(0) - 0xD800) * 0x400
-      + (emoji.charCodeAt(1) - 0xDC00) + 0x10000
-    );
     if (comp < 0) {
       comp = emoji.charCodeAt(0);
     }
