523f5c1d0c01d2ef8800de97dd7b62105f8c2ac0
Morimura Sintaro <snoopy.850807@gmail.com>
2021-01-07 15:03:56 +0800

    adding btncolor

---
+++ src/app/models/btn-color.ts
@@ -0,0 +1,7 @@
+export class BtnColor{
+    most_top_color: string = '#77cc6d';
+    most_down_color: string = '#77cc6d';
+    top_color: string = '#abeba4';
+    down_color: string = '#abeba4';
+  }
+  
